03a4f0ac5ec080b31b8c30c9e617829c0954d30a
BVFreak <42485355+BVFreak@users.noreply.github.com>
2022-07-29 20:57:08 +1000

    updated prizm.py

--- prizm.py
+++ prizm.py
@@ -1,17 +1,28 @@
 import pygame
 
+BACKGROUND_IMAGE = 'bob.png'
+WHITE = (255, 255, 255)
+
 pygame.init()
 
 pygame.mouse.set_cursor(*pygame.cursors.arrow)
-
 screen = pygame.display.set_mode((800, 450))
 caption = pygame.display.set_caption("Pygame")
 clock = pygame.time.Clock()
 
-color = (255, 255, 255)
+# make the background
+screen.fill(WHITE)
 
-screen.fill(color)
+bg_image = pygame.image.load(BACKGROUND_IMAGE)
+
+screen.blit(bg_image, (0, 0))
+
+
 pygame.display.flip()
+
+
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
