5d66d4c0d43626192e5756f07465b55f2622a1ac
BudgieBirb <92903372+BudgieBirb@users.noreply.github.com>
2022-08-05 17:19:49 +1000

    Update prizm.py

--- prizm.py
+++ prizm.py
@@ -1,32 +1,40 @@
 import pygame
 
-BACKGROUND_IMAGE = 'prizm.png'
-WHITE = (255, 255, 255)
-
 pygame.init()
 
-pygame.mouse.set_cursor(*pygame.cursors.arrow)
-screen = pygame.display.set_mode((800, 450))
+(width, height) = (800, 450)
+screen = pygame.display.set_mode((width, height))
 caption = pygame.display.set_caption("Prizm")
 clock = pygame.time.Clock()
 
-# make the background
-screen.fill(WHITE)
+#Loading Images
+bob_surface = pygame.image.load('sprites/bob.png')
 
-bg_image = pygame.image.load(BACKGROUND_IMAGE)
-
-screen.blit(bg_image, (0, 0))
-
-
-pygame.display.flip()
-
-
-
+x_position = 0
+y_position = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+            clock.tick(60)
+
+
+    #Keyboard Input
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        x_position = x_position + 1
+    if keys[pygame.K_LEFT]:
+        x_position = x_position - 1
+    if keys[pygame.K_UP]:
+        y_position = y_position - 1
+    if keys[pygame.K_DOWN]:
+        y_position = y_position + 1
+        
+    #Background/Drawing Image
+    screen.fill((255, 255, 255))
+    
+    screen.blit(bob_surface,(x_position, y_position))
+
     pygame.display.update()
-    clock.tick(60)
