a681d9fcfebd7df49c40639afca4dd4f4ee875c9
BudgieBirb <92903372+BudgieBirb@users.noreply.github.com>
2022-08-31 12:15:54 +1000

    Update prizm.py

--- prizm.py
+++ prizm.py
@@ -6,54 +6,69 @@ from button import Button
 
 pygame.init()
 
+# creates the screen
 width, height = 1280, 720
 screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Prizm")
 
+# fps cap
 clock = pygame.time.Clock()
 
-BACKGROUND_MENU_IMAGE = pygame.image.load("assets/Background-Menu.png")
-BACKGROUND_WORLD_IMAGE = pygame.image.load("assets/background-world.png")
+# menu music
+BACKGROUND_MENU_IMAGE = pygame.image.load("assets/Background-Menu.png").convert()
+BACKGROUND_WORLD_IMAGE = pygame.image.load("assets/background-world.png").convert()
 
-bob_surface = pygame.image.load("assets/bob.png")
+# Bob lefty bobby
+bob_surface = pygame.image.load("assets/bob.png").convert_alpha()
+normal_zomboi_surface = pygame.image.load('assets/normal_zomboi.png').convert_alpha()
 bob_surface_left = pygame.transform.flip(bob_surface, True, False)
 
+
+# sounds
 sound_shoot = pygame.mixer.Sound("assets/audio/pewpew.mp3")
 sound_beep1 = pygame.mixer.Sound("assets/audio/beep1.mp3")
 sound_beep2 = pygame.mixer.Sound("assets/audio/beep2.mp3")
 
-MUSIC_MAINMENU = "assets/audio/song1.mp3"
-MUSIC_MAINMENU1 = "assets/audio/song.mp3"
+# holy music
+MUSIC_MAINMENU1 = "assets/audio/song1.mp3"
 
+# menu colours
 BACKGROUND_MENU = "black"
 BACKGROUND_GAME = "white"
 BACKGROUND_SETTINGS = "lightblue"
 
 
+# menu font
 def get_font(size):  # Returns Font in the desired size
     return pygame.font.Font("assets/font.ttf", size)
 
-
+# stops music
 def play():
     pygame.mixer.music.stop()
 
+    # movement variables
     x_position = (width/2)-50
     y_position = (height/2)-50
-    x_increment = 2
-    y_increment = 2
+    x_increment = 0.5
+    y_increment = 0.5
 
-    x_world_position = -2000
-    y_world_position = -2000
+# world generation size
+    x_world_position = -5000
+    y_world_position = -5000
 
+    # bob righty righthousand
     facing_right = True
     flip = False
     bob = bob_surface
 
+    # while true loop
     while True:
         screen.fill(BACKGROUND_GAME)
 
         for event in pygame.event.get():
             pass
 
+    # returning back to the main menu
         keys = pygame.key.get_pressed()
         if keys[pygame.K_ESCAPE]:
             pygame.mixer.music.play()
@@ -67,7 +82,7 @@ def play():
             if not facing_right:
                 facing_right = True 
                 bob = bob_surface
-            
+        # world moving         
         if keys[pygame.K_a]:
             x_world_position = x_world_position + x_increment
 
@@ -99,7 +114,9 @@ def play():
             if keys[pygame.K_s]:
                 y_world_position = y_world_position - y_increment
             
-
+            enemies = [normal_zomboi_surface]
+            random.choice(enemies)
+     
         screen.blit(BACKGROUND_WORLD_IMAGE, (x_world_position, y_world_position))
         screen.blit(bob, (x_position, y_position))
 
@@ -113,7 +130,7 @@ def settings():
 
         mouse_pos = pygame.mouse.get_pos()
 
-        SETTINGS_TEXT = get_font(50).render("Controlsrender\nWASD to move\nShift to sprint\nLeft click to attack\nRight click to defend\nEscape to go back to main menu\n\nMade by Blake VernerW\nith special thanks to Anthony Tassonyi, Ruan Verner and David Nugent.\n", True, "#999999")
+        SETTINGS_TEXT = get_font(75).render("SETTINGS", True, "#999999")
         SETTINGS_RECT = SETTINGS_TEXT.get_rect(center=(640, 100))
 
         RETURN_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550),
@@ -140,13 +157,8 @@ def settings():
 
 def main_menu():
     pygame.mixer.music.stop()
-    choice_music = random.randint(1, 2)
-    if choice_music == 1:
-        pygame.mixer.music.load(MUSIC_MAINMENU)
-        pygame.mixer.music.play()
-    elif choice_music == 2:
-        pygame.mixer.music.load(MUSIC_MAINMENU1)
-        pygame.mixer.music.play()
+    pygame.mixer.music.load(MUSIC_MAINMENU1)
+    pygame.mixer.music.play()
 
     while True:
         screen.fill(BACKGROUND_MENU)
